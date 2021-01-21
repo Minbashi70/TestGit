6e1d040aa7b5926bda8c84dc4999ee92f982cb7e
M.Minbashi <mo.minbashi@gmail.com>
2021-01-21 13:05:55 +0330

    for second time

--- TestGit/src/main/java/com/Hajilou.java
+++ TestGit/src/main/java/com/Hajilou.java
@@ -1,4 +1,13 @@
 package main.java.com;
 
 public class Hajilou {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- TestGit/src/main/java/com/Minbashi.java
+++ TestGit/src/main/java/com/Minbashi.java
@@ -1,4 +1,13 @@
 package main.java.com;
 
 public class Minbashi {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
