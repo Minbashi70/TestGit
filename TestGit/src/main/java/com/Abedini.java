63d5eee77c469b6b7b69bcdf5293cfde83efbf7f
M.Hajilou <m.hajilou@gmail.com>
2021-01-21 13:24:54 +0330

    add name to Abedini

--- TestGit/src/main/java/com/Abedini.java
+++ TestGit/src/main/java/com/Abedini.java
@@ -2,4 +2,14 @@ package main.java.com;
 
 public class Abedini {
 
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
