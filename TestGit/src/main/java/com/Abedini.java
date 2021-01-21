3d59a2d9ea00e056e08b3b60cca47a890c040b54
M.Minbashi <m.minbashi@gmail.com>
2021-01-21 13:27:04 +0330

    add lastname to Abedini

--- TestGit/src/main/java/com/Abedini.java
+++ TestGit/src/main/java/com/Abedini.java
@@ -1,5 +1,13 @@
 package main.java.com;
 
 public class Abedini {
+    private String lastName;
 
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
